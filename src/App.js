5c398d5555ea8740c820f78da686e68e314c2c57
sisithaJWA <sisitha@jwareautomation.com>
2023-04-10 08:45:59 +0530

    page navigation halfway through

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, { Suspense } from "react";
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/Navbar";
-
+import Profile from './components/Profile';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <Suspense>
+        <Routes>
+          <Route path="/profile" component={Profile} />
+        </Routes>
+
+      </Suspense>
+
     </div>
   );
 }
