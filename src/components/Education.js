c02922d013b1174714b773d82939aa5f3b8b11bc
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-07-02 22:42:29 +0530

    adjusted alignments

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,9 +7,9 @@ export default function Education() {
                 <div className='e-card' >
                     <img src="uoklogo.png" alt="Avatar" id='school-image' />
                     <div className='uok'>
-                        <div className="name-duration" class="flex p-2">
+                        <div className="name-duration" class="flex justify-between p-2">
                             <p class="text-2xl font-bold">University of Kelaniya, Sri Lanka</p>
-                            <p class="text-sm font-bold m-auto p-1 text-green-600 bg-green-200 rounded-md">2019-present</p>
+                            <p class="text-sm font-bold p-1 text-green-600 bg-green-200 rounded-md">2019-present</p>
                         </div>
 
                         <p className='basi-details-2' class="text-lg p-2 text-gray-600">BSc. (Hons.) in Software Engineering</p>
@@ -17,6 +17,7 @@ export default function Education() {
                         <div className="domains" class="flex flex-wrap">
                             <p class="text-md font-bold text-blue-700 hover:text-white bg-white border border-solid border-blue-700 rounded-md m-1 p-1 hover:bg-blue-700">Net Centric Applications</p>
                             <p class="text-md font-bold text-blue-700 hover:text-white bg-white border border-solid border-blue-700 rounded-md m-1 p-1 hover:bg-blue-700">Mobile Computing Applications</p>
+                            <p class="text-md font-bold text-blue-700 hover:text-white bg-white border border-solid border-blue-700 rounded-md m-1 p-1 hover:bg-blue-700">Data Science and Engineering Applications</p>
                         </div>
                     </div>
 
@@ -24,9 +25,9 @@ export default function Education() {
                 <div className='e-card'>
                     <img src="tc.png" alt="Avatar" id='school-image' />
                     <div className='tc'>
-                        <div className="name-duration" class="flex p-2">
-                            <p class="text-2xl font-bold">Thurstan College, Colombo 3, Sri Lanka</p>
-                            <p class="text-sm font-bold m-auto p-1 text-green-600 bg-green-200 rounded-md">2008-2017</p>
+                        <div className="name-duration" class="flex justify-between p-2">
+                            <p class="text-2xl font-bold">Thurstan College, Colombo 3</p>
+                            <p class="text-sm font-bold p-1 text-green-600 bg-green-200 rounded-md">2008-2017</p>
                         </div>
 
                         <p className='basi-details-2' class="text-lg p-2 text-gray-600">G.C.E. Advanced Level - Mathematics Stream</p>
