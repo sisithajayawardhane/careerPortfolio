a53a64ee9674b21c095c7b331c8049582b405ce7
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-07-19 19:01:52 +0530

    alignment were adjusted

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -4,7 +4,7 @@ export default function Education() {
     return (
         <div className="Education" style={{ backgroundColor: 'black',}}>
             <div className="education_cards" class="block xl:flex" >
-                <div class='lg: w-[750px] xl:w-[630px] text-center shadow-[0_4px_8px_0_rgba(0, 0, 0, 0.2)] transition-[0.3s] bg-[white] text-[black] m-[50px] p-[50px] rounded-[5px]'  >
+                <div class='lg: w-[750px] xl:w-[630px] text-center shadow-[0_4px_8px_0_rgba(0, 0, 0, 0.2)] transition-[0.3s] bg-[white] text-[black] m-[20px] p-[50px] rounded-[5px]'  >
                     <img src="uoklogo.png" alt="Avatar" id='school-image' />
                     <div className='uok'>
                         <div className="name-duration" class="flex justify-between p-2">
@@ -37,7 +37,7 @@ export default function Education() {
                     </div>
 
                 </div>
-                <div class='lg: w-[750px] xl:w-[630px] text-center shadow-[0_4px_8px_0_rgba(0, 0, 0, 0.2)] transition-[0.3s] bg-[white] text-[black] m-[50px] p-[50px] rounded-[5px] [w-750px] w1200:[w-630px]'>
+                <div class='lg: w-[750px] xl:w-[630px] text-center shadow-[0_4px_8px_0_rgba(0, 0, 0, 0.2)] transition-[0.3s] bg-[white] text-[black] m-[20px] p-[50px] rounded-[5px] [w-750px] w1200:[w-630px]'>
                     <img src="tc.png" alt="Avatar" id='school-image' />
                     <div className='tc'>
                         <div className="name-duration" class="flex justify-between p-2">
@@ -81,7 +81,7 @@ export default function Education() {
                     <img src="coursera.png" alt="Avatar" class="w-[600px] mx-auto"/>
                     <p class="text-xl font-bold">React Basics By META</p>
                     <p class="text-lg p-2 text-gray-600">Coursera</p>
-                    <p class="text-lg p-2 text-gray-600">Credenntail ID : AT4VKYD69PXR</p>
+                    <p class="text-lg p-2 text-gray-600">Credential ID : AT4VKYD69PXR</p>
                     <p class="text-lg p-2 text-gray-600">Issued Apr 2023</p>
                     <a class="text-md font-bold text-blue-500 hover:text-blue-900" href="https://coursera.org/verify/AT4VKYD69PXR" > Verify</a>
                 </div>
@@ -89,7 +89,7 @@ export default function Education() {
                     <img src="atlassian.png" alt="Avatar" class="w-[200px] mx-auto" />
                     <p class="text-xl font-bold">Jira Fundamentals Badge</p>
                     <p class="text-lg p-2 text-gray-600">Atlassian</p>
-                    <p class="text-lg p-2 text-gray-600">Credenntail ID : 252616295</p>
+                    <p class="text-lg p-2 text-gray-600">Credential ID : 252616295</p>
                     <p class="text-lg p-2 text-gray-600">Issued Apr 2023</p>
                     <a class="text-md font-bold text-blue-500 hover:text-blue-900" href="https://university.atlassian.com/student/award/7xJg6Q4rkTHgAYKmtZGw1nij" > Verify</a>
                 </div>
@@ -97,7 +97,7 @@ export default function Education() {
                     <img src="alison.jpeg" alt="Avatar" class="w-[200px] mx-auto" />
                     <p class="text-xl font-bold">Diploma in Design Thinking</p>
                     <p class="text-lg p-2 text-gray-600">Alison</p>
-                    <p class="text-lg p-2 text-gray-600">Credenntail ID : 1566-17490847</p>
+                    <p class="text-lg p-2 text-gray-600">Credential ID : 1566-17490847</p>
                     <p class="text-lg p-2 text-gray-600">Issued Aug 2020</p>
                     <a class="text-md font-bold text-blue-500 hover:text-blue-900" href="https://alison.com/certification/check/$2y$10$CTGHazH4bHc463RvMRvt5uNc8S3UIdegHLl1RNwnO3Ok9AYO.Ic0." > Verify</a>
                 </div>
@@ -105,7 +105,7 @@ export default function Education() {
                     <img src="alison.jpeg" alt="Avatar" class="w-[200px] mx-auto" />
                     <p class="text-xl font-bold">Diploma in HTML5, CSS3 and JavaScript</p>
                     <p class="text-lg p-2 text-gray-600">Alison</p>
-                    <p class="text-lg p-2 text-gray-600">Credenntail ID : 1462-17490847</p>
+                    <p class="text-lg p-2 text-gray-600">Credential ID : 1462-17490847</p>
                     <p class="text-lg p-2 text-gray-600">Issued Aug 2020</p>
                     <a class="text-md font-bold text-blue-500 hover:text-blue-900" href="https://alison.com/certification/check/$2y$10$NlmXtwMGJ0Lyqvez.ny06ek1.0cFam6ATgM4PcCH82t77I2LpuhpK" > Verify</a>
                 </div>
