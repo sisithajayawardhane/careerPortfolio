f88e2f1dc47ce529576655a2fc0549d81748226e
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-07-19 19:01:34 +0530

    mistakes were corrected

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -16,7 +16,7 @@ export default function Portfolio() {
                     </div>
                     <p class="text-xl font-bold">React Basics By META</p>
                     <p class="text-lg p-2 text-gray-600">Coursera</p>
-                    <p class="text-lg p-2 text-gray-600">Credenntail ID : AT4VKYD69PXR</p>
+                    <p class="text-lg p-2 text-gray-600">Credential ID : AT4VKYD69PXR</p>
                     <p class="text-lg p-2 text-gray-600">Issued Apr 2023</p>
                     <a class="text-md font-bold text-blue-500 hover:text-blue-900" href="https://coursera.org/verify/AT4VKYD69PXR" > Verify</a>
                 </div>
@@ -24,7 +24,7 @@ export default function Portfolio() {
                     <img src="atlassian.png" alt="Avatar" class="w-[200px] mx-auto" />
                     <p class="text-xl font-bold">Jira Fundamentals Badge</p>
                     <p class="text-lg p-2 text-gray-600">Atlassian</p>
-                    <p class="text-lg p-2 text-gray-600">Credenntail ID : 252616295</p>
+                    <p class="text-lg p-2 text-gray-600">Credential ID : 252616295</p>
                     <p class="text-lg p-2 text-gray-600">Issued Apr 2023</p>
                     <a class="text-md font-bold text-blue-500 hover:text-blue-900" href="https://university.atlassian.com/student/award/7xJg6Q4rkTHgAYKmtZGw1nij" > Verify</a>
                 </div>
@@ -32,7 +32,7 @@ export default function Portfolio() {
                     <img src="alison.jpeg" alt="Avatar" class="w-[200px] mx-auto" />
                     <p class="text-xl font-bold">Diploma in Design Thinking</p>
                     <p class="text-lg p-2 text-gray-600">Alison</p>
-                    <p class="text-lg p-2 text-gray-600">Credenntail ID : 1566-17490847</p>
+                    <p class="text-lg p-2 text-gray-600">Credential ID : 1566-17490847</p>
                     <p class="text-lg p-2 text-gray-600">Issued Aug 2020</p>
                     <a class="text-md font-bold text-blue-500 hover:text-blue-900" href="https://alison.com/certification/check/$2y$10$CTGHazH4bHc463RvMRvt5uNc8S3UIdegHLl1RNwnO3Ok9AYO.Ic0." > Verify</a>
                 </div>
@@ -40,7 +40,7 @@ export default function Portfolio() {
                     <img src="alison.jpeg" alt="Avatar" class="w-[200px] mx-auto" />
                     <p class="text-xl font-bold">Diploma in HTML5, CSS3 and JavaScript</p>
                     <p class="text-lg p-2 text-gray-600">Alison</p>
-                    <p class="text-lg p-2 text-gray-600">Credenntail ID : 1462-17490847</p>
+                    <p class="text-lg p-2 text-gray-600">Credential ID : 1462-17490847</p>
                     <p class="text-lg p-2 text-gray-600">Issued Aug 2020</p>
                     <a class="text-md font-bold text-blue-500 hover:text-blue-900" href="https://alison.com/certification/check/$2y$10$NlmXtwMGJ0Lyqvez.ny06ek1.0cFam6ATgM4PcCH82t77I2LpuhpK" > Verify</a>
                 </div>
